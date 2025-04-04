aeacc7a97dc7760f74108bf3f4772eb04daa10bc
Edwin Divine <omegbuedwin@gmail.com>
2025-04-04 18:41:31 +0100

    updated svg.d.ts

--- svg.d.ts
+++ svg.d.ts
@@ -1,8 +1,7 @@
-// types/svg.d.ts
+// src/types/svg.d.ts
 declare module '*.svg' {
     import * as React from 'react';
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
+    const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    export default ReactComponent;
 }
   
