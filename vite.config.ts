7ec5c11b0c4e13df81a64044aa0f1a2c5ea02bed
Edwin Divine <omegbuedwin@gmail.com>
2025-04-04 15:08:36 +0100

    test fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path'; 
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
@@ -9,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@components': normalizePath.resolve(__dirname, 'src/components'),
+      '@components': path.resolve(__dirname, 'src/components'),
     },
   },
 })
